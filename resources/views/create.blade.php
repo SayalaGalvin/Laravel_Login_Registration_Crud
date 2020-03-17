56f27079c523ab7fa5150ea334b7fddc21001aae
hasinimadhuwnthi <hasinimadhuwnthi1234@gmail.com>
2020-03-17 20:36:36 +0530

    First Commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,51 @@
+
+@extends('home')
+
+@section('main')
+@if($errors->any())
+<div class="alert alert-danger">
+ <ul>
+  @foreach($errors->all() as $error)
+  <li>{{ $error }}</li>
+  @endforeach
+ </ul>
+</div>
+@endif
+<div align="right">
+ <a href="{{ route('crud.index') }}" class="btn btn-default">Back</a>
+</div>
+
+<form method="post" action="{{ route('crud.store') }}" enctype="multipart/form-data">
+{{ csrf_field() }}
+ <div class="form-group">
+  <label class="col-md-4 text-left">Enter First Name</label>
+  <div class="col-md-8">
+   <input type="text" name="first_name" class="form-control input-lg" />
+  </div>
+ </div>
+ <br />
+ <br />
+ <br />
+ <div class="form-group">
+  <label class="col-md-4 text-left">Enter Last Name</label>
+  <div class="col-md-8">
+   <input type="text" name="last_name" class="form-control input-lg" />
+  </div>
+ </div>
+ <br />
+ <br />
+ <br />
+ <div class="form-group">
+  <label class="col-md-4 text-left">Select Profile Image</label>
+  <div class="col-md-8">
+   <input type="file" name="image" />
+  </div>
+ </div>
+ <br /><br /><br />
+ <div class="form-group text-center">
+  <input type="submit" name="add" class="btn btn-primary input-lg" value="Add" />
+ </div>
+
+</form>
+
+@endsection
